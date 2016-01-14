560908571ea4bb38856fe040d914dd8b30ffeef2
Xaver Maierhofer <xaver.maierhofer@xwissen.info>
2016-01-14 15:45:09 +0100

    [BUGFIX] OSX bash color

--- utils/beforeStart.sh
+++ utils/beforeStart.sh
@@ -5,7 +5,7 @@ PATCH_BROWSERSYNC=$3
 cd ${WEB_PATH}
 if [ ! -f ./www/shopware.php ]; then
   echo -e "You need a shopware instace in your www folder"
-  echo -e "Provide your repository e.g. git@gitlab.com:company/your-shopware5.git \e[1mOR\e[0m"
+  echo -e "Provide your repository e.g. git@gitlab.com:company/your-shopware5.git \033[1mOR\033[0m"
   echo -e "leave it empty to get defined Shopware version (Configuration.yaml) from github (requires php)."
   echo -e "Project will be cloned into www."
   echo -e "This step deletes content from www. (Crtl-c to stop)"
@@ -16,11 +16,11 @@ if [ ! -f ./www/shopware.php ]; then
     mkdir -p ./www/html
   else
     if ! [ -x "$(command -v php)" ]; then
-      echo -e "\e[0;37m\e[41mYou need to install shopware composer and php with gd and curl\e[0m"
+      echo -e "\033[0;37m\033[41mYou need to install shopware composer and php with gd and curl\033[0m"
       exit 1
     fi
     if ! php -i | grep -q 'GD Support' || ! php -i | grep -q 'cURL support' ; then
-      echo -e "\e[0;37m\e[41mPhp extensions gd and curl missing or not enabled\e[0m"
+      echo -e "\033[0;37m\033[41mPhp extensions gd and curl missing or not enabled\033[0m"
       exit 1
     fi
     rm -rf ./www
